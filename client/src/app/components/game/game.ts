7223d4eb2e2d9be9d5299020e962430a52a5ff0f
arctan2 <prateekbm005@gmail.com>
2025-08-04 21:35:22 +0530

    added snap of planks into place

--- client/src/app/components/game/game.ts
+++ client/src/app/components/game/game.ts
@@ -32,37 +32,41 @@ export class Game {
 	}
 
 	projectCoordToMoveCellId(rIdx: number, cIdx: number) {
-		return `${Math.floor(rIdx / 2)}|${Math.floor(cIdx / 2)}`;
+		return this.toId(Math.floor(rIdx / 2), Math.floor(cIdx / 2));
 	}
 
 	ngOnInit() {
 		const hash: { [_: string]: Player } = {};
 
 		for (const player of this.game.players) {
-			hash[`${player.y}|${player.x}`] = player;
+			hash[this.toId(player.y, player.x)] = player;
 		}
 
 		this.playerPosHash = hash;
 	}
 
-	getWidthHeightFromEl(el: ElementRef) {
-		const { width: widthStr, height: heightStr } = getComputedStyle(el.nativeElement);
+	getWidthHeightFromEl(el: HTMLElement) {
+		const { width: widthStr, height: heightStr } = getComputedStyle(el);
 		const height = Number(heightStr.slice(0, -2));
 		const width = Number(widthStr.slice(0, -2));
 
 		return { width, height };
 	}
 
+	setCursorYX(y: number, x: number) {
+		const cursorEl = this.cursorRef.nativeElement;
+		cursorEl.style.left = `${x}px`;
+		cursorEl.style.top = `${y}px`;
+	}
+
 	placeCursorOnMouse(e: PointerEvent, el: ElementRef) {
 		const board = this.boardContainer.nativeElement;
-		const cursorEl = el.nativeElement;
-		const { width, height } = this.getWidthHeightFromEl(el);
+		const { width, height } = this.getWidthHeightFromEl(el.nativeElement);
 
 		const x = e.x - board.offsetLeft - (width / 2);
 		const y = e.y - board.offsetTop - (height / 2);
 
-		cursorEl.style.left = `${x}px`;
-		cursorEl.style.top = `${y}px`;
+		this.setCursorYX(y, x);
 	}
 
 	setCursor(name: string, el: HTMLElement, isDown: boolean) {
@@ -87,6 +91,15 @@ export class Game {
 	setCursorElId(id: string) {
 		const cur = this.curCursor()
 		cur.el.id = id;
+	}
+
+	toId(rIdx: number, cIdx: number, prefix: string = "") {
+		return `${prefix}i${rIdx}_${cIdx}`;
+	}
+
+	idToIdx(str: string) {
+		const o = str.replace("i", "").split("_").map(Number);
+		return { r: o[0], c: o[1] };
 	}
 
 	onPointerDown(e: PointerEvent) {
@@ -124,9 +137,77 @@ export class Game {
 		this.selectedElement?.classList.remove("selected");
 	}
 
+	handlePlankMove(e: PointerEvent, target: HTMLElement) {
+		const cur = this.curCursor();
+		const { width: targetWidth, height: targetHeight } = this.getWidthHeightFromEl(target);
+		const board = this.boardContainer.nativeElement;
+		const x = e.x - target.offsetLeft - board.offsetLeft;
+		const y = e.y - target.offsetTop - board.offsetTop;
+		const { r, c } = this.idToIdx(target.id);
+
+		if(target.classList.contains("horizontal")) {
+			cur.el.id = "plank-cursor-horizontal";
+
+			if(x < targetWidth / 2) { // less than
+				const element = board.querySelector(
+					this.toId(r, Math.max(c - 2, 0), "#")
+				) as HTMLElement;
+
+				this.setCursorYX(element.offsetTop, element.offsetLeft);
+			} else { // greater than
+				if(c >= this.game.board.board.length - 1) {
+					const element = board.querySelector(
+						this.toId(r, Math.max(c - 2, 0), "#")
+					) as HTMLElement;
+					this.setCursorYX(element.offsetTop, element.offsetLeft);
+				} else {
+					this.setCursorYX(target.offsetTop, target.offsetLeft);
+				}
+			}
+		} else if(target.classList.contains("vertical")) {
+			cur.el.id = "plank-cursor-vertical";
+
+			if(y < targetHeight / 2) { // less than
+				const element = board.querySelector(
+					this.toId(Math.max(r - 2, 0), c, "#")
+				) as HTMLElement;
+				this.setCursorYX(element.offsetTop, element.offsetLeft);
+			} else { // greater than
+				if(r >= this.game.board.board.length - 1) {
+					const element = board.querySelector(
+						this.toId(Math.max(r - 2, 0), c, "#")
+					) as HTMLElement;
+					this.setCursorYX(element.offsetTop, element.offsetLeft);
+				} else {
+					this.setCursorYX(target.offsetTop, target.offsetLeft);
+				}
+			}
+		} else {
+			if(cur.el.id === "plank-cursor-horizontal") {
+				const element = board.querySelector(
+					this.toId(r, Math.max(c - 1, 0), "#")
+				) as HTMLElement;
+				this.setCursorYX(element.offsetTop, element.offsetLeft);
+			} else {
+				const element = board.querySelector(
+					this.toId(Math.max(r - 1, 0), c, "#")
+				) as HTMLElement;
+				this.setCursorYX(element.offsetTop, element.offsetLeft);
+			}
+		}
+	}
+
 	onPointerMove(e: PointerEvent) {
 		if(this.curCursor().el !== null) {
 			this.placeCursorOnMouse(e, this.cursorRef);
+		}
+
+		const target = e.target as HTMLElement | null;
+		const cur = this.curCursor();
+		if(!target || cur.el === null) return;
+
+		if(target.classList.contains("plank-cell")) {
+			this.handlePlankMove(e, target);
 		}
 	}
 
